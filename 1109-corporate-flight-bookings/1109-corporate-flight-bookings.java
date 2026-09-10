f44a2859a6510a10dc85f841a193275f14728aaf
sirishaneti23 <sirishaneti23@gmail.com>
2026-09-10 14:34:08 +0530

    Time: 3 ms (98.21%), Space: 84.5 MB (73.65%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
+        /*
         int[] res = new int[n];
 
         for(int[] book : bookings)
@@ -10,5 +11,23 @@ class Solution {
             }
         }
         return res;
+        */
+        int[] ans = new int[n];
+        for(int[] booking: bookings)
+        {
+            int first = booking[0];
+            int last = booking[1];
+            int seats = booking[2];
+            ans[first-1] +=seats;
+            if(last < n)
+            {
+                ans[last] -= seats;
+            }
+        }
+        for(int i = 1; i < n; i++)
+        {
+            ans[i] += ans[i-1];
+        }
+        return ans;
     }
 }
