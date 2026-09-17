6362d6ee8d511fb3fb4c3dda2d65c0931ed44b88
sirishaneti23 <sirishaneti23@gmail.com>
2026-09-17 15:36:22 +0530

    Time: 14 ms (96.02%), Space: 98 MB (46.68%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -5,7 +5,8 @@ class Solution {
         backtrack(1, n, k, current);
         return result;
     }
-
+    /*
+    //backtrack
     public void backtrack(int start, int n, int k, List<Integer> current)
     {
         if(current.size() == k)
@@ -15,6 +16,24 @@ class Solution {
         }
 
         for(int i = start; i <= n; i++)
+        {
+            current.add(i);
+            backtrack(i+1, n, k, current);
+            current.remove(current.size() - 1);
+        }
+    }
+    */
+    
+    //backtrack and pruning
+    public void backtrack(int start, int n, int k, List<Integer> current)
+    {
+        if(current.size() == k)
+        {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        int remaining = k - current.size();
+        for(int i = start; i <= n - remaining + 1; i++)
         {
             current.add(i);
             backtrack(i+1, n, k, current);
